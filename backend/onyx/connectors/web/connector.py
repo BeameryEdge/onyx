0883eea52be8414ae1f7200d6e79b77ddb20be81
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2025-03-03 22:52:57 +0100

    Playwright fixes to fix crawling certain websites

--- backend/onyx/connectors/web/connector.py
+++ backend/onyx/connectors/web/connector.py
@@ -162,7 +162,7 @@ def get_internal_links(
 
 def start_playwright() -> Tuple[Playwright, BrowserContext]:
     playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
 
     context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
